2903ec546dcfb9444a8f45cab6857c224898d298
kosuke-morikawa <haoyousenchuan@gmail.com>
2025-12-02 02:02:20 +0900

    Usernameに変更

--- laravel/resources/js/Pages/Auth/Login.jsx
+++ laravel/resources/js/Pages/Auth/Login.jsx
@@ -34,7 +34,7 @@ export default function Login({ status, canResetPassword }) {
             <form onSubmit={submit}>
                 {/* ユーザーネーム入力 */}
                 <div>
-                    <InputLabel htmlFor="name" value="Usersname" />
+                    <InputLabel htmlFor="name" value="Username" />
 
                     <TextInput
                         id="name"
